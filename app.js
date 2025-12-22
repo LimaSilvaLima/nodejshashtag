bff74923f91898ca42ce5a909d0f7387ce88397a
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-12-22 14:43:49 -0300

    concluido endpoint get de detalhes time por sigla

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const app = express();
 
 /* Endpoint to get the 2024 league table 
 app.get('/tabela', (req, res) => {
-  res.json(tabela2024);
+  res.json(tabela2024);                                                                                                                                                                    
 });
 */
 
 app.get('/', (req, res) => {
-  res.send(tabela2024);
+  res.status().send(tabela2024);
+});
+
+app.get('/:sigla', (req, res) => {
+  const siglaInformada = req.params.sigla.toUpperCase();
+  const time = tabela2024.find((infoTime) => infoTime.sigla === siglaInformada);
+  if (!time) {
+    return res.status(404).send('Time não encontrado');
+  }
+  res.send(time);
 });
 
 app.listen(3000, () => {
