cd4c03bceef6659ba5688c56ffb1e6ac95ce9e90
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-12-21 21:46:26 -0300

    primeira versao do servidor com rota get rodando

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 import tabela2024 from "./tabela.js";
 import express from 'express';
-  
+ 
+const app = express();
+
+/* Endpoint to get the 2024 league table 
+app.get('/tabela', (req, res) => {
+  res.json(tabela2024);
+});
+*/
+
+app.get('/', (req, res) => {
+  res.send(tabela2024);
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
